435f0a9e7732aacdfcf222291cd86982bf9ed8ea
yuko <yuriii.kovalyov@gmail.com>
2024-04-29 16:06:12 +0300

    Added vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
 });
